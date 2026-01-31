235eb1f56af50dfab50656a0b4f26aa49ee89cd2
Matej <hozlarmatej0@gmail.com>
2026-01-31 15:31:18 +0100

    dump schema

--- db/schema/init.sql
+++ db/schema/init.sql
@@ -1,6 +1,6 @@
 -- Auto-generated schema initialization file
 -- This file sources all individual type, table and function files
--- Generated at: 2026-01-29T14:54:18.180Z
+-- Generated at: 2026-01-31T14:30:56.596Z
 
 -- ============================================================================
 -- CUSTOM TYPES (ENUMS)
