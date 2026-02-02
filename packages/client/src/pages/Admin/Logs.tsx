37650a427897411561b4c3f188a27db0802e0070
Matej <hozlarmatej0@gmail.com>
2026-02-02 10:28:13 +0100

    feat(client): add admin page boilerplates

---
+++ packages/client/src/pages/Admin/Logs.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../Profile/Profile.module.scss";
+
+export const AdminLogs: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Admin - Logs</h1>
+      <p>Admin Logs page coming soon...</p>
+    </div>
+  );
+};
---
+++ packages/client/src/pages/Admin/Messages.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../Profile/Profile.module.scss";
+
+export const AdminMessages: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Admin - Messages</h1>
+      <p>Admin Messages page coming soon...</p>
+    </div>
+  );
+};
