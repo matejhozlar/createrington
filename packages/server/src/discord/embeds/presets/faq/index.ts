4901c16d8f8678715674259dcd4e719b6a525851
Matej <hozlarmatej0@gmail.com>
2026-03-18 16:12:43 +0100

    refactor(server): replace FAQ welcome embed fields with inline channel mentions

--- packages/server/src/discord/embeds/presets/faq/index.ts
+++ packages/server/src/discord/embeds/presets/faq/index.ts
@@ -6,44 +6,23 @@ import { Discord } from "@/discord/constants";
 export const FaqEmbedPresets = {
   /** Pinned welcome embed with useful channel links for the questions channel */
   welcomeMessage() {
+    const ch = Discord.Channels;
+    const m = ch.mention.bind(ch);
+
+    const channels = [
+      m(ch.createringtonOfficial.SUPPORT),
+      m(ch.createringtonOfficial.RULES),
+      m(ch.general.COMMANDS),
+      m(ch.createringtonOfficial.ANNOUNCEMENTS),
+      m(ch.createringtonOfficial.DOWNLOAD),
+      m(ch.createringtonOfficial.ROLES),
+    ].join("  ");
+
     return createEmbed()
       .title("Welcome to Questions!")
-      .description("Have a question? Just type it here and we'll try to help!")
-      .field(
-        "Support",
-        Discord.Channels.mention(
-          Discord.Channels.createringtonOfficial.SUPPORT,
-        ),
-        true,
-      )
-      .field(
-        "Rules",
-        Discord.Channels.mention(Discord.Channels.createringtonOfficial.RULES),
-        true,
-      )
-      .field(
-        "Commands",
-        Discord.Channels.mention(Discord.Channels.general.COMMANDS),
-        true,
-      )
-      .field(
-        "Announcements",
-        Discord.Channels.mention(
-          Discord.Channels.createringtonOfficial.ANNOUNCEMENTS,
-        ),
-        true,
-      )
-      .field(
-        "Download",
-        Discord.Channels.mention(
-          Discord.Channels.createringtonOfficial.DOWNLOAD,
-        ),
-        true,
-      )
-      .field(
-        "Roles",
-        Discord.Channels.mention(Discord.Channels.createringtonOfficial.ROLES),
-        true,
+      .description(
+        "Have a question? Just type it here and we'll try to help!\n\n" +
+          `**Useful channels**\n${channels}`,
       )
       .color(EmbedColors.Info);
   },
