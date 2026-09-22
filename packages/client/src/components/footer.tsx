0382608690e8a8b72f5e6248201e802d85130f68
matejhozlar <hozlarmatej0@gmail.com>
2026-09-23 01:23:35 +0200

    fix(client): spread footer link columns evenly on wide layouts

--- packages/client/src/components/footer.tsx
+++ packages/client/src/components/footer.tsx
@@ -37,7 +37,7 @@ export const Footer = () => {
   return (
     <footer className="@container w-full border-t bg-background px-5 md:px-8 py-12">
       <div className="max-w-7xl mx-auto">
-        <div className="grid grid-cols-2 gap-8 @xl:grid-cols-4 @4xl:grid-cols-[1fr_repeat(4,auto)]">
+        <div className="grid grid-cols-2 gap-8 @xl:grid-cols-4 @4xl:grid-cols-[3fr_repeat(4,1fr)]">
           <div className="flex flex-col col-span-2 gap-2 @xl:col-span-4 @4xl:col-span-1 @4xl:justify-center">
             <NavLink to="/" className="flex items-center gap-3">
               <img
